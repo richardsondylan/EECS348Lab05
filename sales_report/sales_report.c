a6e355f927c94d8508bd298b039bbe983c81fd74
Dylan Richardson <d347r606@ku.edu>
2026-03-03 14:27:03 -0600

    Completed sales report function

--- sales_report/sales_report.c
+++ sales_report/sales_report.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
+
+
+void generate_sales_reports(float *arr, int size){
+
+    const char* months[] = {
+        "January", "Februrary", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December" 
+    };
+
+    if(size != 12){
+        printf("Error: Passed array of size not equal to 12. Not full annual report.\n");
+    }
+    else {
+        printf("Monthly Sales Report for 2024\n\n");
+        printf("%-10s Sales\n", "Month");
+        for(int i = 0; i < size; ++i){
+            printf("%-10s %f\n", months[i], arr[i]);
+        }
+    }
+
+
+}
+
+
 int main(){
 
-    
+    float arr[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    generate_sales_reports(arr, 12);
     return 0;
 }
