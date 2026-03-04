fee785444ee195abe683d754b4d1ae3441393e1e
Dylan Richardson <d347r606@ku.edu>
2026-03-04 00:54:22 -0600

    all functions added in sales reporting. Need more specification on input (TO BE ASKED IN LAB)

--- sales_report/sales_report.c
+++ sales_report/sales_report.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 
 void generate_sales_reports(float *arr, int size){
@@ -80,13 +80,69 @@ void compute_six_month_moving_average(float *arr, int size){
     } 
 }
 
+struct month_sale_pair{
+    char *month;
+    float sales;
+} typedef month_sale_pair;
+
+
+int compare(const void *month_sale_pair1, const void *month_sale_pair2){
+    return ((month_sale_pair*)month_sale_pair2)->sales - ((month_sale_pair*)(month_sale_pair1))->sales;
+}
+
+
+void sort_sales_report(float *arr, int size){
+
+    const char* months[] = {
+        "January", "Februrary", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December" 
+    };
+
+    if(size != 12){
+        printf("Error: Passed array of size not equal to 12. Not full annual report.\n");
+    }
+    else {
+        printf("Sales report (highest to lowest)\n");
+        printf("%-10s Sales\n", "Month");
+        month_sale_pair new_arr[size];
+        for(int i = 0; i < size; ++i){
+            month_sale_pair pair = {months[i], arr[i]};
+            new_arr[i] = pair;
+        }
+        qsort(new_arr, 12, sizeof(month_sale_pair), compare);
+        
+        for(int i = 0; i < size; ++i){
+            printf("%-10s %f\n", new_arr[i].month, new_arr[i].sales);
+        }
+
+        printf("\n");
+    }
+
+}
+
+void read_input_file(char *file_name, float *arr, int size){
+    FILE *file;
+    file = fopen(file_name, "r");
+    if(file == NULL){
+        printf("Could not open file.\n");
+    }
+    else {
+        fread(arr, sizeof(float), 12, file);
+        for(int i = 0; i < size; ++i){
+            printf("%f\n", arr[i]);
+        }
+    }
+
+
+}
+
 
 
 int main(){
 
     float arr[] = {23458.01,40122,56011.85,37820,37904,60200,72400,56210,67230,68233,80950,95225};
-    generate_sales_reports(arr, 12);
-    compute_sales_report_stats(arr, 12);
-    compute_six_month_moving_average(arr, 12);
+    float arr1[12];
+    read_input_file("test.txt", arr1, 12);
+
     return 0;
 }
